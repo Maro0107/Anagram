d673ae58b9308a0a552881f1113f5bdaaefc4608
Uzytkownik <uzytkownik@kursGIT.pl>
2022-08-17 18:31:41 +0200

    Changes

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(isAnagramSort("aaabbbcccdjeeee", "aaabbbcccdeeeee"));
+
+    }
+
+    static boolean isAnagramSort(String string1, String string2) {
+        if (string1.length() != string2.length()) {
+            return false;
+        }
+        char[] a1 = string1.toCharArray();
+        char[] a2 = string2.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        return Arrays.equals(a1, a2);
+    }
+}
